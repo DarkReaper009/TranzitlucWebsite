c66cb56909ac72f9916519284ac51e47b4376c75
DarkReaper009 <gabias.akaly@gmail.com>
2025-09-26 20:01:22 +0300

    happy clients move

--- src/components/HappyClients.tsx
+++ src/components/HappyClients.tsx
@@ -1,6 +1,11 @@
 // import React from "react"
 // import ReactDOM from "react-dom/client"
 import './HappyClients.css';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+//IMAGES
 import sofidel from '../assets/Images/sofidel_logo.svg';
 import stepone from '../assets/Images/stepone_logo.svg';
 import petstar from '../assets/Images/petstar_logo.svg';
@@ -12,10 +17,60 @@ const HappyClients = () => {
       <p className="text-sh2 text-white font-sh2 tracking-[2px]">
         CLIENTI MULTUMITI
       </p>
-      <div className="w-full overflow-x-auto h-40 flex items-center  ">
+      {/* <div className="slider-wrapper w-full overflow-x-auto h-40 flex items-center  "> */}
+      <Swiper
+        modules={[Navigation, Pagination]}
+        direction="horizontal"
+        slidesPerView={4}
+        spaceBetween={20}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          320: { slidesPerView: 1 }, // mobile
+          640: { slidesPerView: 2 }, // small tablets
+          1024: { slidesPerView: 4 }, // desktop
+        }}
+        className="w-full h-40"
+      >
+        <SwiperSlide>
+          <div className="flex items-center justify-center h-full">
+            <img className="w-[18.3rem] h-20" src={sofidel} alt="" />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="flex items-center justify-center h-full">
+            <img className="w-[18.3rem] h-20" src={stepone} alt="" />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="flex items-center justify-center h-full">
+            <img className="w-[18.3rem] h-20" src={petstar} alt="" />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="flex items-center justify-center h-full">
+            <img className="w-[18.3rem] h-20" src={elis} alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex items-center justify-center h-full">
+            <img className="w-[18.3rem] h-20" src={elis} alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="flex items-center justify-center h-full">
+            <img className="w-[18.3rem] h-20" src={elis} alt="" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      {/* </div> */}
+      {/* <div className="slider-wrapper w-full overflow-x-auto h-40 flex items-center  ">
         <ul
-          //   style={{ listStyle: 'none' }}
-          className="flex flex-nowrap gap-7 overflow-x-auto scrollbar-hide h-40 items-center list-none"
+       
+          className="card-list flex flex-nowrap gap-7 overflow-x-auto scrollbar-hide h-40 items-center list-none"
         >
           <li className="min-w-[18.3rem]">
             <img className="w-[18.3rem] h-20" src={sofidel} alt="" />
@@ -39,7 +94,7 @@ const HappyClients = () => {
             <img className="w-[18.3rem] h-20" src={petstar} alt="" />
           </li>
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 };
